7656105daa5e730dbb953e9e2a34e512428e1409
Alysson-Araujo <alysson10araujo@gmail.com>
2022-06-17 22:46:09 -0300

    first commit

---
+++ modulo javascript/projeto introducao pratica ao typescript/mentoria-typescript-main/src/minhas-respostas/desafio1.ts
@@ -0,0 +1,13 @@
+// /**
+//  * Uma maneira de resolver o problema 1 que pensei seria criar uma interface para o let employee, impondo os
+//  * valores que são obrigatórios para ele. Essa é uma forma de fazer, existe outras mais simples.
+//  */
+
+// interface IEmployee {
+//     code:number,
+//     name:string
+// }
+
+// let employee: IEmployee = {code:10, name: "John"};
+
+// console.log(employee)
